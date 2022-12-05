c20f36d777f33d160e91eb669d867500949ef7ec
xforcevesa <101607521+xforcevesa@users.noreply.github.com>
2022-12-05 15:11:45 +0800

    Create build_script.sh

---
+++ build_script.sh
@@ -0,0 +1,15 @@
+if [ ! -d "./riscv-isa-manual" ]
+then
+  git https://github.com/riscv/riscv-isa-manual
+  cd riscv-isa-manual
+else
+  cd riscv-isa-manual
+  git pull
+fi
+
+if ! pdflatex -v &> /dev/null
+then
+  sudo dnf install texlive{,-multirow,-comment,-verbatimbox} -y
+fi
+cd build
+make
